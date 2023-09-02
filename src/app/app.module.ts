e8ca37ab7e2ae5990388f72ba083623a05c0b82a
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-09-02 11:38:43 -0500

    add sign-up and sign-in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInComponent } from './app/security/pages/sign-in/sign-in.component';
+import { SingUpComponent } from './app/security/pages/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SingUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
