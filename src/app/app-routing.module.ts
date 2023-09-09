48c9d679f8d8c6d4cf68e8d813149a457767cd40
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-09-09 10:37:39 -0500

    fix! app.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SingUpComponent } from './app/security/pages/sing-up/sing-up.component';
-import { SignInComponent } from './app/security/pages/sign-in/sign-in.component';
+import { SignUpComponent } from './security/pages/sign-up/sign-up.component';
+import { SignInComponent } from './security/pages/sign-in/sign-in.component';
 
 
 const routes: Routes = [
-  { path: "sign-up", component: SingUpComponent },
+  { path: "sign-up", component: SignUpComponent },
   { path: "sign-in", component: SignInComponent },
 
   { path: "**", component: SignInComponent }
