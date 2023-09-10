750d1465069bff22155f9ab20d791843a387ab4f
cvasquez31 <138951439+cvasquez31@users.noreply.github.com>
2023-09-10 08:45:29 -0500

    fix(sign-in): html css

--- src/app/security/pages/sign-in/sign-in.component.ts
+++ src/app/security/pages/sign-in/sign-in.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit{
+
+  hide: boolean = true;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+  }
+
+  loginForm: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
 
 
-  onSubmit() {
-
+  onLogin() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    console.log(this.loginForm.value);
   }
 
 }
