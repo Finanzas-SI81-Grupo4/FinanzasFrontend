ae634c369d9757c87f0d033d9216a7124ec1c3b7
cvasquez31 <138951439+cvasquez31@users.noreply.github.com>
2023-09-10 08:40:28 -0500

    fix(sign-up): html css

--- src/app/security/pages/sign-up/sign-up.component.ts
+++ src/app/security/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { matchpassword } from './matchpassword.validator';
+
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+
+  hide: boolean = true;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+  }
+
+  signUpForm: FormGroup = this.formBuilder.group({
+    firstName: ['',[Validators.required]],
+    lastName: ['',[Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    cpassword: ['', [Validators.required]],
+  },
+  { 
+    validators:matchpassword
+  })
+
+
+  onRegister() {
+    if (!this.signUpForm.valid) {
+      return;
+    }
+    console.log(this.signUpForm.value);
+  }
 
 }
