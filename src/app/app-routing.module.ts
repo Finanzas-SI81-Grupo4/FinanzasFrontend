46f482e61d051d3e00ed92c9a3c823bd30f09fa1
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-11-08 20:40:37 -0500

    (Routes): fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './security/pages/sign-up/sign-up.component';
 import { SignInComponent } from './security/pages/sign-in/sign-in.component';
+import {HomeComponent} from "./components/home/home.component";
 
 
 const routes: Routes = [
   { path: "sign-up", component: SignUpComponent },
   { path: "sign-in", component: SignInComponent },
+  {path:"home", component:HomeComponent },
 
   { path: "**", component: SignInComponent }
 ];
