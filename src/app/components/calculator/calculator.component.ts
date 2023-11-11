96b58cc2631f78c6a93fda9f6e815576d46840e5
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-11-11 17:00:00 -0500

    (calculator) fixed service

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Cronograma} from "../../models/Cronograma";
 import {CronogramaService} from "../../services/cronograma.service";
 
@@ -7,12 +7,23 @@ import {CronogramaService} from "../../services/cronograma.service";
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit{
 
   cronograma: Cronograma = new Cronograma();
   customerId: number = 1; // Reemplaza con el valor correcto
 
   constructor(private cronogramaService: CronogramaService) {}
+
+  ngOnInit() {
+    // Obtén el valor del customerId desde el sessionStorage
+    const userId = sessionStorage.getItem('userid');
+
+    if (userId) {
+      this.customerId = parseInt(userId, 10);
+    } else {
+      this.customerId = 1; // Valor predeterminado si no hay usuario en sesión
+    }
+  }
 
   enviarDatos(): void {
     this.cronogramaService
@@ -25,5 +36,10 @@ export class CalculatorComponent {
           console.error('Error al enviar datos:', error);
         }
       );
+  }
+
+  logout() {
+    sessionStorage.removeItem('userid');
+    console.log('FUNCIONAAAAAA');
   }
 }
